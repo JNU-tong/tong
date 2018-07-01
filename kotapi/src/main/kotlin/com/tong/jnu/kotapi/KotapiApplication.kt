50fc5c9836ad0b6c5a472ca85c8e11f287ebe6f1
replaykim <replaykim123@gmail.com>
2018-07-01 17:01:31 +0900

    api module init

---
+++ kotapi/src/main/kotlin/com/tong/jnu/kotapi/KotapiApplication.kt
@@ -0,0 +1,11 @@
+package com.tong.jnu.kotapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotapiApplication>(*args)
+}
